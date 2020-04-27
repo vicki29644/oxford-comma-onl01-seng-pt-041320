6db89923722620a81d64f6b2b90370ee81b7a2a1
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-27 02:22:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+if array.length == 1 
+  array[0]
+elsif array.length == 2
+  array.join(" and ")  
+else
+  array[-1] = "and #{array[-1]}"
+  array.join(", ")
+ binding.pry 
+end
 end
